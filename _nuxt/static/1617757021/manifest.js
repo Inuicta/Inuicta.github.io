9a789354f44df72c705307e7207d71f97f3f77b8
Jonathan Crum <crumja@uga.edu>
2021-04-06 21:01:14 -0400

    First commit

---
+++ _nuxt/static/1617757021/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fart","\u002Fstore","\u002Fdesign","\u002Fdesign-gallery\u002Fse_landscapes","\u002Fdesign-gallery\u002Frainbow_star","\u002Fdesign-gallery\u002Fclassic_city_eats","\u002Fdesign-gallery\u002Flowe_cal","\u002Fdesign-gallery\u002Fhomestar","\u002Fdesign-gallery\u002Flcuga5"]})
