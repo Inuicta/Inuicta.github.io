aecf68529495b55e8968573087df887af02da590
krummja <crumja4@gmail.com>
2021-04-12 19:19:09 -0400

    .

---
+++ _nuxt/static/1618269175/design-gallery/homestar/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618269175",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:"\u002Fdesign-gallery\u002Fhomestar",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d8810aa8"}}}}(null));
