cf25f2e160f761c22da4bfc62082cb8ac8911d3b
krummja <crumja4@gmail.com>
2021-04-22 19:40:12 -0400

    Updating site design

--- _nuxt/static/1618269175/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618269175",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d8810aa8"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1619134793/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdesign","\u002Fstore","\u002Fart","\u002Fdesign-gallery\u002Flowe_cal","\u002Fdesign-gallery\u002Fclassic_city_eats","\u002Fdesign-gallery\u002Fhomestar","\u002Fdesign-gallery\u002Fse_landscapes","\u002Fdesign-gallery\u002Frainbow_star","\u002Fdesign-gallery\u002Flcuga5"]})
