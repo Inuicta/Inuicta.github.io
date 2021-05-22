552a7a3058b3c6ba310c4deca9a69722dce2b44f
Inuicta <crumja4@gmail.com>
2021-05-22 17:15:09 -0400

    Fixing CNAME

--- _nuxt/static/1621716566/design/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621716566",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:"\u002Fdesign",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"80f97a80"}}}}(null));
---
+++ _nuxt/static/1621718056/art-gallery/ufo_attack/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621718056",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:"\u002Fart-gallery\u002Fufo_attack",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"33ff40a6"}}}}(null));
