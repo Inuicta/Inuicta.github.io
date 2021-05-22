ebb23acb72005ceb3094ba3889cabf51c8dcdaa8
krummja <crumja4@gmail.com>
2021-05-22 16:49:49 -0400

    Updating art gallery

--- _nuxt/static/1620612096/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fart","\u002Fstore","\u002Fdesign","\u002Fdesign-gallery\u002Fse_landscapes","\u002Fdesign-gallery\u002Flowe_cal","\u002Fdesign-gallery\u002Flcuga5","\u002Fdesign-gallery\u002Fclassic_city_eats","\u002Fdesign-gallery\u002Fhomestar","\u002Fdesign-gallery\u002Frainbow_star"]})
