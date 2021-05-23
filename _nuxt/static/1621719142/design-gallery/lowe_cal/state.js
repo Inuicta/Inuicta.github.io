b5ceee2a7a4c1493b7f390c4de39b2acd5a06a40
Inuicta <crumja4@gmail.com>
2021-05-23 16:46:57 -0400

    Updating front page

--- _nuxt/static/1621719142/design-gallery/lowe_cal/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621719142",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:"\u002Fdesign-gallery\u002Flowe_cal",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7a1fd0cd"}}}}(null));
---
+++ _nuxt/static/1621802776/design-gallery/classic_city_eats/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621802776",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:"\u002Fdesign-gallery\u002Fclassic_city_eats",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ec6cc797"}}}}(null));
