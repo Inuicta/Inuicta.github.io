687df915c294949e6fcc7c11ab6c1d1d7800e191
Inuicta <crumja4@gmail.com>
2021-05-24 15:23:57 -0400

    .

--- _nuxt/static/1621883745/design-gallery/se_landscapes/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621883745",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:"\u002Fdesign-gallery\u002Fse_landscapes",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"656e9d46"}}}}(null));
---
+++ _nuxt/static/1621884218/art-gallery/postapoc/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621884218",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:"\u002Fart-gallery\u002Fpostapoc",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9bd4e083"}}}}(null));
