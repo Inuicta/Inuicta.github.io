4c8222a393735dac12688879cc044fabf4fb3f18
Inuicta <crumja4@gmail.com>
2021-05-24 15:06:41 -0400

    Fixing form

--- _nuxt/static/1621882490/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621882490",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"36fe532b"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1621883174/store/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621883174",layout:"default",error:a,state:{drawer:{drawer:false}},serverRendered:true,routePath:"\u002Fstore",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d6bcfd4"}}}}(null));
